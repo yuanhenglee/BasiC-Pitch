378c6afb69a4c157938cf5aedb7acf97762ea73b
yuanhenglee <yuanhenglee2000@gmail.com>
2023-11-19 21:25:40 +0800

    add: incorporate eigen & fftw b,

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-./clean.sh  
+
+./clean.sh
+
+if [ ! -d "src/ext/eigen-3.3.7" ] || [ ! -d "src/ext/fftw-3.3.10" ]; then
+    cur_wd=$(pwd)
+    cd src/ext
+    tar -zxf eigen-3.3.7.tar.gz  # eigen do not need to install
+    tar -zxf fftw-3.3.10.tar.gz
+    cd fftw-3.3.10
+    ./configure --enable-shared --enable-float  -prefix=${PWD}/build 
+    make CFLAGS="-fPIC"
+    make install
+    cd ${cur_wd}
+fi
+
 mkdir build
 cd build
 cmake ..
