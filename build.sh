82c71f6b19d735c6e00c6eb41892ca6b3861c9fd
yuanhenglee <yuanhenglee2000@gmail.com>
2023-11-20 18:59:09 +0800

    fix: use local eigen instead code from tar file

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 
 ./clean.sh
 
-if [ ! -d "src/ext/eigen-3.3.7" ] || [ ! -d "src/ext/fftw-3.3.10" ]; then
+if [ ! -d "src/ext/fftw-3.3.10" ]; then
+    if [ ! -f "src/ext/fftw-3.3.10.tar.gz" ]; then
+        wget http://www.fftw.org/fftw-3.3.10.tar.gz -P src/ext
+    fi
     cur_wd=$(pwd)
     cd src/ext
-    tar -zxf eigen-3.3.7.tar.gz  # eigen do not need to install
     tar -zxf fftw-3.3.10.tar.gz
+    rm fftw-3.3.10.tar.gz
     cd fftw-3.3.10
     ./configure --enable-shared --enable-float  -prefix=${PWD}/build 
     make CFLAGS="-fPIC"
