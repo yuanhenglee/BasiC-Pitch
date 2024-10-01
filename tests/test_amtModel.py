3871d383f0357fbc6514336a4b913f35b4696da4
yuanhenglee <yuanhenglee2000@gmail.com>
2024-10-01 22:50:56 +0800

    fix: update test scripts due to changes in basic-pitch API

--- tests/test_amtModel.py
+++ tests/test_amtModel.py
@@ -1,11 +1,14 @@
 import numpy as np
 
+
 def get_audio(shorten=False):
     import librosa
-    sig, _  = librosa.load("data/Undertale-Megalovania.wav", sr=22050, mono=True)
+
+    sig, _ = librosa.load("data/Undertale-Megalovania.wav", sr=22050, mono=True)
     if shorten:
-        sig = sig[:sig.shape[0] // 4]
+        sig = sig[: sig.shape[0] // 4]
     return sig
+
 
 def test_amtModelCQ():
     import BasiCPP_Pitch
@@ -14,29 +17,34 @@ def test_amtModelCQ():
     t = bp_model.getCQ()
 
     lf = t.getFilter()
-    gold = np.load('model/lowpass_filter.npy')
+    gold = np.load("model/lowpass_filter.npy")
 
     assert np.allclose(lf, gold)
 
     kernel = t.getKernel()
-    gold = np.load('model/kernel.npy')
+    gold = np.load("model/kernel.npy")
 
     assert np.allclose(kernel, gold)
 
-def plot_hm( datas ):
+
+def plot_hm(datas):
     import matplotlib.pyplot as plt
-    plt.figure(figsize=( 8*2, 6 ))
+
+    plt.figure(figsize=(8 * 2, 6))
     for i, prefix in enumerate(["Baseline", "Ours"]):
         for j, suffix in enumerate(["contour", "note", "onset"]):
-            plt.subplot(3, 2, i+2*j + 1)
+            plt.subplot(3, 2, i + 2 * j + 1)
             plt.title(prefix + " " + suffix)
             v = datas[prefix + " " + suffix]
             v = (v - v.min()) / (v.max() - v.min())
-            plt.imshow(v.T, cmap='coolwarm', vmin=0, vmax=1, aspect='auto', origin='lower')
+            plt.imshow(
+                v.T, cmap="coolwarm", vmin=0, vmax=1, aspect="auto", origin="lower"
+            )
     plt.tight_layout()
     plt.savefig("./tests/model_output.png")
 
-def print_distribution( datas ):
+
+def print_distribution(datas):
     for k, v in datas.items():
         print(k, v.shape)
         print("min", v.min())
@@ -45,7 +53,8 @@ def print_distribution( datas ):
         print("std", v.std())
         print("")
 
-def test_inference(vis = False):
+
+def test_inference(vis=False):
     import BasiCPP_Pitch
     import time
 
@@ -58,23 +67,24 @@ def test_inference(vis = False):
     Yp, Yn, Yo = bp_model.getOutput()
     print(f"Elapsed time: {time.time() - start_time}")
 
-    plot_dict['Ours contour'] = np.array(Yp)
-    plot_dict['Ours note'] = np.array(Yn)
-    plot_dict['Ours onset'] = np.array(Yo)
+    plot_dict["Ours contour"] = np.array(Yp)
+    plot_dict["Ours note"] = np.array(Yn)
+    plot_dict["Ours onset"] = np.array(Yo)
 
     start_time = time.time()
     import os
+
     os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-    os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+    os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
     # force 1 thread
-    os.environ['TF_NUM_INTEROP_THREADS'] = '1'
-    os.environ['TF_NUM_INTRAOP_THREADS'] = '1'
+    os.environ["TF_NUM_INTEROP_THREADS"] = "1"
+    os.environ["TF_NUM_INTRAOP_THREADS"] = "1"
     from basic_pitch.inference import run_inference
     from basic_pitch import ICASSP_2022_MODEL_PATH
     from tensorflow import saved_model
-    model = saved_model.load(str(ICASSP_2022_MODEL_PATH))
-    gold = run_inference("data/Undertale-Megalovania.wav", model)
+
+    gold = run_inference("data/Undertale-Megalovania.wav", ICASSP_2022_MODEL_PATH)
     print(f"Elapsed time: {time.time() - start_time}")
     for k, v in gold.items():
         plot_dict["Baseline " + k] = v
@@ -82,8 +92,12 @@ def test_inference(vis = False):
     # print_distribution(plot_dict)
 
     for output in ["onset", "contour", "note"]:
-        assert plot_dict["Baseline " + output].shape == plot_dict["Ours " + output].shape
-        assert np.allclose(plot_dict["Baseline " + output], plot_dict["Ours " + output], atol=1e-3)
+        assert (
+            plot_dict["Baseline " + output].shape == plot_dict["Ours " + output].shape
+        )
+        assert np.allclose(
+            plot_dict["Baseline " + output], plot_dict["Ours " + output], atol=1e-3
+        )
 
     if vis:
         plot_hm(plot_dict)
@@ -92,3 +106,4 @@ def test_inference(vis = False):
 if __name__ == "__main__":
     test_inference(vis=True)
     # test_amtModelCQ()
+
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,14 @@
 import numpy as np
 
+
 def get_audio(shorten=False):
     import librosa
-    sig, _  = librosa.load("data/Undertale-Megalovania.wav", sr=22050, mono=True)
+
+    sig, _ = librosa.load("data/Undertale-Megalovania.wav", sr=22050, mono=True)
     if shorten:
-        sig = sig[:sig.shape[0] // 4]
+        sig = sig[: sig.shape[0] // 4]
     return sig
+
 
 def test_matConversion():
     from BasiCPP_Pitch.utils import testMatConversion
@@ -15,6 +18,7 @@ def test_matConversion():
     mat = testMatConversion(np_in)
 
     assert np.allclose(mat, np_in)
+
 
 def test_windowed_audio():
     from BasiCPP_Pitch.utils import getWindowedAudio
@@ -26,13 +30,22 @@ def test_windowed_audio():
     print(np_out.shape)
 
     import warnings
+
     warnings.simplefilter("ignore")
     with warnings.catch_warnings():
         from basic_pitch.inference import get_audio_input
-        gold, _, _ = get_audio_input("data/Undertale-Megalovania.wav", 30*256, 43844-30*256)
-        gold = gold.numpy().squeeze()
-        print(gold.shape)
-    assert np.allclose(np_out, gold)
+
+        # gold, _, _ = get_audio_input("data/Undertale-Megalovania.wav", 30*256, 43844-30*256)
+        input = get_audio_input(
+            "data/Undertale-Megalovania.wav", 30 * 256, 43844 - 30 * 256
+        )
+        for i, (gold, _, _) in enumerate(input):
+            gold = gold.squeeze()
+            out = np_out[i, :]
+            print(out.shape)
+            print(gold.shape)
+            assert np.allclose(out, gold)
+
 
 if __name__ == "__main__":
     # test_matConversion()
